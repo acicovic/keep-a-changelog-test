99f24287896b51ac88bf9fd432ced68d66e3a31f
acicovic <acicovic@users.noreply.github.com>
2022-05-09 13:24:33 +0000

    Update plugin version to 1.0.11

--- tests/e2e/utils.js
+++ tests/e2e/utils.js
@@ -1,6 +1,6 @@
 import { activatePlugin, loginUser, visitAdminPage } from '@wordpress/e2e-test-utils';
 
-export const PLUGIN_VERSION = '1.0.10';
+export const PLUGIN_VERSION = '1.0.11';
 
 export const waitForWpAdmin = () => page.waitForSelector( 'body.wp-admin' );
 
