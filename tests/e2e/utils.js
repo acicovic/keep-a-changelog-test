1c249ddf5f256723c88dc5a9ff04df93f7f7a27e
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2022-05-12 13:50:43 +0300

    Update utils.js

--- tests/e2e/utils.js
+++ tests/e2e/utils.js
@@ -1,6 +1,6 @@
 import { activatePlugin, loginUser, visitAdminPage } from '@wordpress/e2e-test-utils';
 
-export const PLUGIN_VERSION = '1.0.36';
+export const PLUGIN_VERSION = '1.0.37';
 
 export const waitForWpAdmin = () => page.waitForSelector( 'body.wp-admin' );
 
