38c1babd8a374b7a21c16fcd53928bcd613e8b37
acicovic <acicovic@users.noreply.github.com>
2022-05-17 07:52:29 +0000

    Bump plugin version number to 1.0.60

--- tests/e2e/utils.js
+++ tests/e2e/utils.js
@@ -1,6 +1,6 @@
 import { activatePlugin, loginUser, visitAdminPage } from '@wordpress/e2e-test-utils';
 
-export const PLUGIN_VERSION = '1.0.59';
+export const PLUGIN_VERSION = '1.0.60';
 
 export const waitForWpAdmin = () => page.waitForSelector( 'body.wp-admin' );
 
