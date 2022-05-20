3fd6df1b64535722353d1ec76572189371637e76
acicovic <acicovic@users.noreply.github.com>
2022-05-20 09:34:47 +0000

    Bump plugin version to 1.0.81 and update changelog

--- tests/e2e/utils.js
+++ tests/e2e/utils.js
@@ -1,6 +1,6 @@
 import { activatePlugin, loginUser, visitAdminPage } from '@wordpress/e2e-test-utils';
 
-export const PLUGIN_VERSION = '1.0.80';
+export const PLUGIN_VERSION = '1.0.81';
 
 export const waitForWpAdmin = () => page.waitForSelector( 'body.wp-admin' );
 
